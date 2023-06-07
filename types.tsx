18087be9a954c85cc2003bd93ff98ba453133bb5
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-08 00:25:51 +0900

    Update types

---
+++ types.tsx
@@ -0,0 +1,10 @@
+export type foodItemStatic = {
+  id: string,
+  title: string,
+  desc: string,
+  price: string,
+  imgSrc: string,
+};
+export type foodItemsStatic = {
+  items: foodItemStatic[];
+}
