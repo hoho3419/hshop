e9f54094e1133734774f6041f663fcca6505d1fd
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-08 00:28:25 +0900

    Fix Env firebase

---
+++ src/firebase.config.js
@@ -0,0 +1,23 @@
+import { initializeApp,getApp,getApps } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage'
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
+
+//  기존에 한번 연결되었는지 확인한다.
+const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
+// Firestore 데이터베이스 인스턴스 객체 반환
+const firestore = getFirestore(app);
+// Storage 인스턴스를 생성 후 반환.
+const storage = getStorage(app);
+
+export { app, firestore, storage };
