446d816c2789e3641eee62fc97da1ac71ae7e78a
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-08 00:31:46 +0900

    Export created Components

---
+++ src/Pages/index.tsx
@@ -0,0 +1,7 @@
+export { default as Home } from './Home';
+export { default as About } from './About';
+export { default as Signup } from './Auth/Register';
+export { default as Login } from './Auth/Login';
+export { default as Profile } from './Profile';
+export { default as Services } from './Services';
+export { default as Menu } from './Menu';
