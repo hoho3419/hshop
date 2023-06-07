7cde890d5be381e82690a4febef8dfbe2a70f497
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-08 00:39:37 +0900

    Setup utils

---
+++ src/utils/showcaseStatic.tsx
@@ -0,0 +1,32 @@
+import {Chicken,Fish,Icream,Strawberry} from '../components/Assets'
+
+export const staticData = [
+  {
+    id: 's1',
+    title: "딸기",
+    desc: '신선한 딸기',
+    price: '10000',
+    imgSrc: Strawberry
+  },
+  {
+    id: 's2',
+    title: "치킨",
+    desc: '맛있는 치킨',
+    price: '20000',
+    imgSrc: Chicken
+  },
+  {
+    id: 's3',
+    title: "생선 구이",
+    desc: '맛있는 생선구이',
+    price: '20000',
+    imgSrc: Fish
+  },
+  {
+    id: 's4',
+    title: "아이스크림",
+    desc: '초콜릿 & 바닐라',
+    price: '8900',
+    imgSrc: Icream
+  }
+]
