823ccb22fee71c5bb0ca0dba22232fdd036265c6
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-08 00:23:01 +0900

    src init

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {
+  Home,
+  About,
+  Login,
+  Menu,
+  Profile,
+  Services,
+  Signup
+} from './Pages';
+import { Header } from './components'
+import { AnimatePresence } from 'framer-motion';
+import { ToastContainer } from 'react-toastify'
+import { Routes,Route } from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AnimatePresence 
+    
+    >
+     <ToastContainer />
+     {/* 오른쪽 카트와 왼쪽 메세지 해야함 */}
+     <div className='w-screen h-auto min-h-[100vh] flex flex-col bg-primary'>
+      <Header />
+      <main
+        className="mt-16 md:mt-16 px-3 md:px-8 md:py-6 py-4"
+      >
+        <Routes>
+          <Route path='/*' element={<Home />}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/register' element={<Signup/>}/>
+          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/about' element={<About/>}/>
+          <Route path='/menu' element={<Menu/>}/>
+          <Route path='/services' element={<Services/>}/>
+        </Routes>
+      </main>
+     </div>
+    </AnimatePresence>
   );
 }
 
