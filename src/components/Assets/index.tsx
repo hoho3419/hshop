ce1e643d1e19f607c0a00b64e5a49cfd8840946b
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-08 00:30:35 +0900

    Export Assets

---
+++ src/components/Assets/index.tsx
@@ -0,0 +1,16 @@
+import AvatarI from '../../img/avatar.png';
+import delivery from '../../img/delivery.png';
+import HeroBgI from '../../img/hero-bg.png';
+import ChickenI from "../../img/c3.png";
+import FishI from "../../img/fi3.png";
+import IcreamI from "../../img/i1.png";
+import StrawberryI from "../../img/f5.png";
+
+export const Avatar = AvatarI;
+export const bikeDelivery = delivery;
+export const HeroBg = HeroBgI;
+
+export const Chicken = ChickenI;
+export const Fish = FishI;
+export const Icream = IcreamI;
+export const Strawberry = StrawberryI;
