f56fa7a72300ae001a15e66de60bf736dc1ffc9c
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-10 22:23:07 +0900

    ShowcaseBanner, FruitsSection, MenuSection, Footer, AssetUploader 컴포넌트 추가

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,2 +1,6 @@
 export { default as Header } from './Header';
 export { default as ShowcaseBanner } from './Showcase';
+export { default as FruitsSection } from './Sections/Fruits';
+export { default as MenuSection } from './Sections/Menu';
+export { default as Footer } from './Footer';
+export { default as AssetUploader } from './Upload'
