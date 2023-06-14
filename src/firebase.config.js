da5b264fdbaf93d9d257f35c9d626f0b729b6ab4
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-14 23:34:36 +0900

    firebase 설정 변경

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -5,14 +5,13 @@ import { getStorage } from 'firebase/storage'
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  // databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  // measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-
 //  기존에 한번 연결되었는지 확인한다.
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
 // Firestore 데이터베이스 인스턴스 객체 반환
