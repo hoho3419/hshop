62a4b3bc20f27b83d818970301a4ed854923300b
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-15 23:45:24 +0900

    Login 상태면 DropDown 되는 컴포넌트 생성

--- src/components/Header/DropDown.tsx
+++ src/components/Header/DropDown.tsx
@@ -2,9 +2,30 @@ import React from 'react';
 import { FaUserCog } from 'react-icons/fa';
 import { MdLogout } from 'react-icons/md';
 import { motion } from 'framer-motion' ;
-import { Link } from 'react-router-dom';
+import { Link,useNavigate } from 'react-router-dom';
+import { firebaseLogout } from '../../firebase';
+import { useRecoilState } from 'recoil';
+import { userState,cartItemsState } from '../../store/recoilState';
 
-const DropDown = ({user}: {user: any }) => {
+const DropDown = () => {
+  const navigate = useNavigate();
+  const [user,setUser] = useRecoilState(userState);
+  const setCartItems = useRecoilState(cartItemsState)[1];
+
+  const logoutHandler = async () => {
+    await firebaseLogout()
+    .then(() => {
+      setUser(null);
+      setCartItems([]);
+      localStorage.setItem('user','undefined');
+      localStorage.removeItem('cartItems');
+      navigate('/');
+    })
+    .catch((err) => {
+      console.log(err.message);
+    })
+  }
+
   return (
     <motion.div
       initial={{opacity: 0, scale: 0.6}}
@@ -22,6 +43,7 @@ const DropDown = ({user}: {user: any }) => {
       </Link>
       <p
         className="cursor-pointer px-10 py-2 flex items-center gap-3 hover:bg-slate-100 transition-all duration-100 ease-in-out text-base text-textColor"
+        onClick={logoutHandler}
       >
         Logout
         <MdLogout />
