ef605a50eb067a171424f0e2a1a631f600e598e2
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-16 22:12:20 +0900

    setstate 변수 이름변경

--- src/components/Header/MobileNav.tsx
+++ src/components/Header/MobileNav.tsx
@@ -14,8 +14,8 @@ const MobileNav = ({
   setIsOpen: any;
 }) => {
   const cartItems = useRecoilState(cartItemsState)[0];
-  const showCart = useRecoilState(showCartState)[1];
-  const showContactForm = useRecoilState(showContactFormState)[1];
+  const setShowCart = useRecoilState(showCartState)[1];
+  const setShowContactForm = useRecoilState(showContactFormState)[1];
 
   return (
     <div className='flex flex-col bg-cardOverlay backdrop-blur-sm items-start justify-start gap-16 w-screen h-screen overflow-y-hidden z-50 overflow-hidden'>
@@ -27,7 +27,7 @@ const MobileNav = ({
           animate={{ opacity: 1, x: 0 }}
           exit={{ opacity: 0, x: 200 }}
           className='relative flex items-center justify-center text-textColor'
-          onClick={() => showCart((prev) => !prev)}
+          onClick={() => setShowCart((prev) => !prev)}
         >
           <MdShoppingBasket className='text-4xl cursor-pointer' />
           {cartItems && (
@@ -60,7 +60,7 @@ const MobileNav = ({
           <Link to={'/about'} onClick={() => setIsOpen(!isOpen)}>
             About
           </Link>
-          <p onClick={() => showContactForm((prev) => !prev)} className='text-base text-textColor cursor-pointer hover:text-headingColor duration-100 transition-all ease-in-out px-10'>
+          <p onClick={() => setShowContactForm((prev) => !prev)} className='text-base text-textColor cursor-pointer hover:text-headingColor duration-100 transition-all ease-in-out px-10'>
             Contact
           </p>
         </div>
