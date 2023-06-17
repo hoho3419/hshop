dd27b3d89ad12a3010281c078946ffd208d8beb9
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-17 23:34:47 +0900

    Cart 컴포넌트 Contact 레이아웃 컴포넌트 추가, 기능 미완성

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "react-toastify/dist/ReactToastify.css";
 import {
   Home,
   About,
@@ -8,19 +9,24 @@ import {
   Services,
   Signup
 } from './Pages';
-import { Header,Footer } from './components'
+import { Header,Footer,Cart,Contact } from './components'
 import { AnimatePresence } from 'framer-motion';
 import { ToastContainer } from 'react-toastify'
 import { Routes,Route } from 'react-router-dom'
 import { v4 as uuidv4 } from 'uuid';
+import { useRecoilState } from 'recoil'
+import { showCartState,showContactFormState } from './store/recoilState';
 
 function App() {
+  const showCart = useRecoilState(showCartState)[0];
+  const showContactForm = useRecoilState(showContactFormState)[0]
   return (
-    <AnimatePresence 
-    >
+    <AnimatePresence>
      <ToastContainer key={uuidv4()}/>
      {/* 오른쪽 카트와 왼쪽 메세지 해야함 */}
      <div className='w-screen h-auto min-h-[100vh] flex flex-col bg-primary'>
+      {showCart && <Cart />}
+      {showContactForm && <Contact />}
       <Header />
       <main
         className="mt-16 md:mt-16 px-3 md:px-8 md:py-6 py-4"
