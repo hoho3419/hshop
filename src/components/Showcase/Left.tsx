71f3f7c6126903909a776d750418d5231ea4c8c6
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-18 23:25:17 +0900

    Text 교체

--- src/components/Showcase/Left.tsx
+++ src/components/Showcase/Left.tsx
@@ -32,7 +32,7 @@ const Left = () => {
         whileHover={{scale: 1.1}}
         className='bg-gradient-to-br from-orange-400 to-orange-500 w-full md:w-auto px-4 py-2 rounded-lg hover:shadow-lg transition-all ease-in-out duration-100'
       >
-        Order
+        주문하기
       </motion.button>
     </div>
   );
