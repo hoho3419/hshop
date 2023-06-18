1f65489c9d8f62faf2f0e3352e8a5b435b65721f
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-18 23:26:25 +0900

    Dummy 데이터 삭제 및 firebase 데이터베이스에서 데이터 가져오기

--- src/components/Sections/Fruits/index.tsx
+++ src/components/Sections/Fruits/index.tsx
@@ -1,93 +1,10 @@
 import React,{ useState } from 'react';
 import { Title,PrevNext as PrevNextButtons } from '../'
 import Container from '../../Container';
-import { Strawberry } from '../../Assets';
-
-const fruit = [
-  {
-    id: 'f1',
-    title: "딸기",
-    description: "딸기",
-    price: "딸기",
-    imageURL: Strawberry,
-    calories: "딸기",
-    qty:"딸기",
-    category: "딸기",
-  },
-  {
-    id: 'f2',
-    title: "딸기",
-    description: "딸기",
-    price: "딸기",
-    imageURL: Strawberry,
-    calories: "딸기",
-    qty:"딸기",
-    category: "딸기",
-  },
-  {
-    id: 'f3',
-    title: "딸기",
-    description: "딸기",
-    price: "딸기",
-    imageURL: Strawberry,
-    calories: "딸기",
-    qty:"딸기",
-    category: "딸기",
-  },
-  {
-    id: 'f4',
-    title: "딸기",
-    description: "딸기",
-    price: "딸기",
-    imageURL: Strawberry,
-    calories: "딸기",
-    qty:"딸기",
-    category: "딸기",
-  },
-  {
-    id: 'f5',
-    title: "딸기",
-    description: "딸기",
-    price: "딸기",
-    imageURL: Strawberry,
-    calories: "딸기",
-    qty:"딸기",
-    category: "딸기",
-  },
-  {
-    id: 'f6',
-    title: "딸기",
-    description: "딸기",
-    price: "딸기",
-    imageURL: Strawberry,
-    calories: "딸기",
-    qty:"딸기",
-    category: "딸기",
-  },
-  {
-    id: 'f7',
-    title: "딸기",
-    description: "딸기",
-    price: "딸기",
-    imageURL: Strawberry,
-    calories: "딸기",
-    qty:"딸기",
-    category: "딸기",
-  },
-  {
-    id: 'f8',
-    title: "딸기",
-    description: "딸기",
-    price: "딸기",
-    imageURL: Strawberry,
-    calories: "딸기",
-    qty:"딸기",
-    category: "딸기",
-  },
-];
+import { FilterFood } from '../../../utils/filters';
 
 const Fruits = () => {
-  // const fruits = 
+  const fruits = FilterFood('fruits');
   const [scrollValue,setScrollValue] = useState(0);
   return (
     <section className='w-full my-5'>
@@ -95,7 +12,7 @@ const Fruits = () => {
         <Title title='Our fresh & healthy fruits'/>
         <PrevNextButtons onNext={() => setScrollValue(10000)} onPrev={() => setScrollValue(-10000)}/>
       </div>
-      <Container className="bg-containerbg" scrollOffset = {scrollValue} items={fruit} />
+      <Container className="bg-containerbg" scrollOffset = {scrollValue} items={fruits} />
     </section>
   );
 };
