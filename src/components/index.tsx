494a6ec306ed40972b57f3cd8f2642013c907554
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-18 23:27:08 +0900

    Cart Contact 컴포넌트 추가

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -3,4 +3,6 @@ export { default as ShowcaseBanner } from './Showcase';
 export { default as FruitsSection } from './Sections/Fruits';
 export { default as MenuSection } from './Sections/Menu';
 export { default as Footer } from './Footer';
-export { default as AssetUploader } from './Upload'
+export { default as AssetUploader } from './Upload';
+export { default as Cart } from './Cart'
+export { default as Contact } from './Contact'
