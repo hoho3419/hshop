f156ce0223ed1e5dc6c667ec5a7a8d0ae94b9003
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-18 23:28:09 +0900

    Contact 컴포넌트 set State 상태 추가

--- src/components/Header/Navigations.tsx
+++ src/components/Header/Navigations.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-router-dom'
 import { MdShoppingBasket } from 'react-icons/md';
+import { useRecoilState } from 'recoil'
+import { showCartState,showContactFormState } from '../../store/recoilState'; 
 
 const Navigations = () => {
+  const setShowCart = useRecoilState(showCartState)[1];
+  const setShowContact = useRecoilState(showContactFormState)[1];
+
   return (
     <div className='flex items-center gap-8'>
       <motion.ul
@@ -39,8 +44,9 @@ const Navigations = () => {
         <motion.li
           whileHover={{scale: 1.1}}
           className='md:text-sm lg:text-md text-base text-textColor cursor-pointer hover:text-headingColor duration-100 transition-all ease-in-out'
+          onClick={() => setShowContact((prev) => !prev)}
         >
-          Contact us
+          Contact me
         </motion.li>
       </motion.ul>
 
@@ -48,6 +54,7 @@ const Navigations = () => {
         whileTap={{scale: 0.9}}
         whileHover={{scale: 1.1}}
         className='relative flex items-center justify-center text-textColor'
+        onClick={() => setShowCart((prev) => !prev)}
       >
         <MdShoppingBasket className='text-2xl cursor-pointer'/>
         <div className='absolute -top-2 -right-2 w-5 h-5 rounded-full bg-cartNumBg flex items-center justify-center cursor-pointer'>
