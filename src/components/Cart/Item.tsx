5ba0a52ca94a82a333471af07a366cf6029b0463
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-20 23:42:46 +0900

    Cart 로직 추가 및 레이아웃 변경

--- src/components/Cart/Item.tsx
+++ src/components/Cart/Item.tsx
@@ -1,22 +1,75 @@
-import React from 'react';
+import React,{useEffect} from 'react';
 import { BiMinus, BiPlus } from "react-icons/bi";
 import { MdDelete } from "react-icons/md";
 import { motion } from 'framer-motion';
-import { cartItem,FoodItem } from '../../../types';
+import { cartItem } from '../../../types';
 import { useRecoilState } from 'recoil';
-import { cartItemsState,foodItemsState } from '../../store/recoilState';
+import { cartItemsState,cartTotalState,foodItemsState } from '../../store/recoilState';
 import { getFoodyById } from '../../utils/functions';
+import { firebaseDeleteCartItem, firebaseUpdateCartItem } from '../../firebase';
 
 
 const CartItem = ({ item } : { item: cartItem }) => {
+  let price:number = 0;
   const foodItems = useRecoilState(foodItemsState)[0];
-  const cartItems = useRecoilState(cartItemsState)[0];
+  const [cartItems,setCartItems] = useRecoilState(cartItemsState);
+  const setTotalPrice = useRecoilState(cartTotalState)[1];
   const { id, fid, qty } = item;
-
   const foodItem = getFoodyById(foodItems, fid);
 
+  if(foodItem){
+    price = +foodItem?.price;
+  }
+
+  const deleteCartItemHandler = async () => {
+    try {
+      await firebaseDeleteCartItem(item);
+      const newItems = cartItems.filter((cartItem) => cartItem.id !== id);
+      setCartItems([...newItems]);
+    } catch (error: any) {
+      console.log(error);
+    }
+  };
+  
+  const updateCartItemQtyHandler = async (val:number) => {
+    const index = cartItems.findIndex((cartItem) => cartItem.id === item.id);
+    if(index !== -1){
+      setCartItems((prevItems) => {
+        const newItems = prevItems.map((el) => {
+          if (el.id === item.id) {
+            return { ...el, qty: el.qty + val };
+          }
+          return el;
+        });
+        firebaseUpdateCartItem(newItems[index])
+          .then(() => {
+            console.log('Firebase 업데이트 성공');
+          })
+          .catch((error) => {
+            console.log(error);
+          });
+        return newItems;
+      });
+    }
+  }
+
+  const calculateCartTotal = () => {
+    const total: number = cartItems.reduce((acc,cartItem) => {
+      const cartFoodItem = getFoodyById(foodItems, cartItem.fid);
+      if (cartFoodItem) {
+        return acc + (+cartItem.qty * +cartFoodItem.price);
+      }
+      return acc;
+    },0);
+    setTotalPrice(total);
+  }
+  
+  useEffect(() => {
+    calculateCartTotal();
+  },[cartItems])
+
   return (
-    <div className='w-full p-1 px-2 rounded-lg bg-cartItem hover:shadow-md flex flex-col items-center justify-between gap-2 cursor-pointer'>
+    <div className='w-full p-1 px-2 rounded-lg bg-cartItem hover:shadow-md flex items-center justify-between gap-2 cursor-pointer'>
       <div className='flex items-center gap-2'>
         <img 
           src={foodItem?.imageURL} 
@@ -25,9 +78,9 @@ const CartItem = ({ item } : { item: cartItem }) => {
         />
 
         <div className='flex flex-col gap-0'>
-          <p className='text-base text-gra'>{foodItem?.title}</p>
+          <p className='text-base text-gray-50'>{foodItem?.title}</p>
           <p className='text-sm block text-gray-300 font-semibold'>
-            <span className='text-xs text-red-600'>₩</span> {foodItem?.price}
+            <span className='text-xs text-red-600'>₩</span> {price.toLocaleString("ko-KR")}
           </p>
         </div>
       </div>
@@ -35,6 +88,7 @@ const CartItem = ({ item } : { item: cartItem }) => {
       <div className='group flex items-center gap-2 cursor-pointer'>
         <motion.div
           whileTap={{ scale: 0.75 }}
+          onClick={qty > 1 ? () => updateCartItemQtyHandler(-1) : () => {}}
         >
           <BiMinus className='text-gray-50' />
         </motion.div>
@@ -43,6 +97,7 @@ const CartItem = ({ item } : { item: cartItem }) => {
         </p>
         <motion.div
           whileTap={{ scale: 0.75 }}
+          onClick={() => updateCartItemQtyHandler(1)}
         >
           <BiPlus className='text-gray-50' />
         </motion.div>
@@ -51,6 +106,7 @@ const CartItem = ({ item } : { item: cartItem }) => {
       <motion.div
         whileTap={{ scale: 0.75 }}
         className='text-sm text-gray-50 w-6 h-6 rounded-lg bg-cartNumBg flex items-center justify-center'
+        onClick={deleteCartItemHandler}
       >
         <MdDelete />
       </motion.div>
--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -2,10 +2,15 @@ import React, { useState } from 'react';
 import Checkout from '../Checkout';
 import { motion } from 'framer-motion'
 import CartHeader from './Header';
+import { useRecoilState } from 'recoil';
+import { cartItemsState } from '../../store/recoilState';
+import CartBody from './Body';
+import EmptyCart from '../EmptyCart';
+import NotFound from '../NotFound';
 
 const Cart = () => {
   const [checkoutOpen,setCheckoutOpen] = useState(false);
-
+  const cartItems = useRecoilState(cartItemsState)[0];
   return (
     <>
      {checkoutOpen ? (
@@ -19,7 +24,15 @@ const Cart = () => {
           className='w-full h-screen md:w-[350px] bg-white md:backdrop-blur-sm flex flex-col z-[101] drop-shadow-xl fixed top-0 right-0'
         >
           <CartHeader />
+          {cartItems && cartItems.length > 0 ? (
+            <CartBody action={setCheckoutOpen}/>
+          ) : (
+            <div className='h-full w-full flex-1 flex items-center justify-center'>
+              <EmptyCart />
+            </div>
+          )}
         </motion.div>
+        {!cartItems && <NotFound text='장바구니를 사용할 수 없습니다' />}
       </>
      )} 
     </>
