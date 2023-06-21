1ee0270f49a13152599bc275a3ba521ca80c1f88
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-21 23:32:38 +0900

    CartItem 가져오는 함수 추가

--- src/utils/fetchSessionData.js
+++ src/utils/fetchSessionData.js
@@ -1,8 +1,16 @@
 export const fetchSessionUser = () => {
   const user =
-    localStorage.getItem("user") !== null
+    localStorage.getItem("user") !== "undefined"
       ? JSON.parse(localStorage.getItem("user"))
       : localStorage.clear();
-      
     return user;
+};
+
+export const fetchSessionCart = () => {
+  const cartInfo =
+    localStorage.getItem("cartItems") !== "undefined"
+      ? JSON.parse(localStorage.getItem("cartItems"))
+      : localStorage.clear();
+
+    return cartInfo ? cartInfo : [];
 };
