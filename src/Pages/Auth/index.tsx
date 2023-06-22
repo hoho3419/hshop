9b2da964e77cd0c295d99528583c0609d92f0ea1
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-22 22:15:11 +0900

    회원가입 google Auth 추가

--- src/Pages/Auth/index.tsx
+++ src/Pages/Auth/index.tsx
@@ -5,16 +5,45 @@ import { toast } from "react-toastify";
 import { BsGithub } from "react-icons/bs";
 import { FcGoogle } from "react-icons/fc";
 import { bikeDelivery } from '../../components/Assets';
+import { useRecoilState } from 'recoil';
+import { userState } from '../../store/recoilState';
+import { AUTHPROVIDER } from '../../firebase';
+import { useNavigate } from 'react-router-dom';
+import { GoogleAuthProvider } from 'firebase/auth';
 
 
 const ProviderAuth = () => {
+  const [user,setUser] = useRecoilState(userState);
+  const navigate = useNavigate();
+  const GOOGLE_PROVIDER = new GoogleAuthProvider();
+
+  const AUTH = async ({ provider } : { provider: any }) => {
+    if(!user){
+      toast.promise(AUTHPROVIDER(provider),{
+        pending: "로그인중...",
+        success: '로그인에 성공했습니다.',
+        error: '에러가 발생했습니다. 다시 시도해주세요!🤗'
+      })
+      .then(({ refreshToken, userData }) => {
+        const SignUser = userData[0];
+        setUser(() => SignUser);
+        localStorage.setItem('user',JSON.stringify(SignUser));
+        navigate('/');
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        toast.error(errorMessage,{autoClose: 1500});
+      })
+    }
+  }
+
   return (
     <div className="flex items-center justify-center gap-5  text-center">
       <motion.p
         whileHover={{ scale: 1.1 }}
         className="flex items-center w-36 h-10 bg-white justify-center rounded text-headingColor px-5 cursor-pointer shadow-sm hover:bg-slate-100"
         onClick={() =>
-          toast.warn("GitHub Signin is not available yet", {
+          toast.warn("아직 미완성 기능입니다!", {
             autoClose: 2000,
             icon: (
               <MdOutlineNotificationsActive className="text-yellow-500 text-xl" />
@@ -29,6 +58,7 @@ const ProviderAuth = () => {
       <motion.p
         whileHover={{ scale: 1.1 }}
         className="flex items-center w-36 h-10 bg-white justify-center rounded text-headingColor px-5 cursor-pointer shadow-sm hover:bg-slate-100"
+        onClick={() => AUTH({ provider: GOOGLE_PROVIDER })}
       >
         <FcGoogle className="text-xl w-5 mr-1" />
         <span>Google</span>
