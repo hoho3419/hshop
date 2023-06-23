7e69f3e6d4d1e18d256369ac0c5fa14f4295fa18
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-23 23:04:56 +0900

    index export 컴포넌트 추가

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -5,4 +5,6 @@ export { default as MenuSection } from './Sections/Menu';
 export { default as Footer } from './Footer';
 export { default as AssetUploader } from './Upload';
 export { default as Cart } from './Cart'
-export { default as Contact } from './Contact'
+export { default as Contact } from './Contact';
+export { default as Loader } from './Loader';
+export { default as GoogleMaps } from './Map';
