2bdd0156279a9038ea0d6ebd4cd63fddf798a65e
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-24 17:00:18 +0900

    각페이지 컴포넌트 조건추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React,{ useEffect } from 'react';
 import "react-toastify/dist/ReactToastify.css";
 import {
   Home,
@@ -12,18 +12,42 @@ import {
 import { Header,Footer,Cart,Contact } from './components'
 import { AnimatePresence } from 'framer-motion';
 import { ToastContainer } from 'react-toastify'
-import { Routes,Route } from 'react-router-dom'
+import { Routes,Route } from 'react-router-dom';
 import { v4 as uuidv4 } from 'uuid';
 import { useRecoilState } from 'recoil'
-import { showCartState,showContactFormState } from './store/recoilState';
+import { cartItemsState, foodItemsState, showCartState,showContactFormState, userState } from './store/recoilState';
+import { firebaseFetchFoodItems } from './firebase'; 
+import { fetchUserCartData } from './utils/functions';
 
 function App() {
+  const user = useRecoilState(userState)[0];
   const showCart = useRecoilState(showCartState)[0];
-  const showContactForm = useRecoilState(showContactFormState)[0]
+  const showContactForm = useRecoilState(showContactFormState)[0];
+  const setCartItems = useRecoilState(cartItemsState)[1];
+  const setFoodItems = useRecoilState(foodItemsState)[1];
+
+  useEffect(() => {
+    const fetchFoodData = async () => {
+      await firebaseFetchFoodItems()
+      .then((data) =>{
+        setFoodItems(data);
+      })
+      .catch((error) => {
+        console.log(error);
+      })
+    };
+    fetchFoodData();
+  },[])
+
+  useEffect(() => {
+    user && fetchUserCartData(user,setCartItems);
+  },[user])
+
+
   return (
-    <AnimatePresence>
-     <ToastContainer key={uuidv4()}/>
+    <AnimatePresence >
      {/* 오른쪽 카트와 왼쪽 메세지 해야함 */}
+     <ToastContainer key={uuidv4()}/>
      <div className='w-screen h-auto min-h-[100vh] flex flex-col bg-primary'>
       {showCart && <Cart />}
       {showContactForm && <Contact />}
@@ -32,10 +56,10 @@ function App() {
         className="mt-16 md:mt-16 px-3 md:px-8 md:py-6 py-4"
       >
         <Routes>
-          <Route path='/*' element={<Home />}/>
-          <Route path='/login' element={<Login/>}/>
-          <Route path='/register' element={<Signup/>}/>
-          <Route path='/profile' element={<Profile/>}/>
+          <Route path='/*' element={<Home />} />
+          <Route path='/login' element={!user ? <Login/> : <Home />}/>
+          <Route path='/register' element={!user ? <Signup/> : <Home />}/>
+          <Route path='/profile' element={user ? <Profile/> : <Login/>} />
           <Route path='/about' element={<About/>}/>
           <Route path='/menu' element={<Menu/>}/>
           <Route path='/services' element={<Services/>}/>
