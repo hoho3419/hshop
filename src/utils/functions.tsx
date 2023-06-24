3b54073505ab8fbdc11a3f33cd726cef4be41222
hoho3419 <106577276+hoho3419@users.noreply.github.com>
2023-06-24 17:00:39 +0900

    function 추가

--- src/utils/functions.tsx
+++ src/utils/functions.tsx
@@ -1,10 +1,11 @@
+import { toast } from "react-toastify";
 import { FoodItem,cartItem } from "../../types";
-import { firebaseEmptyUserCart } from "../firebase";
-
+import { firebaseEmptyUserCart, firebaseFetchAllCartItems, firebaseUpdateUser } from "../firebase";
+// foodItem 반환
 export const getFoodyById = (menu: FoodItem[], fid: number) => {
   return menu.find((item: FoodItem) => item.id === fid);
 };
-
+// 아이템 섞어서 반환하기
 export const shuffleItems = (items: any) => {
   let currentIndex = items.length;
   let randomIndex;
@@ -24,7 +25,7 @@ export const shuffleItems = (items: any) => {
 
   return items;
 };
-
+// 장바구니 비우기 
 export const emptyCart = async (
   cartItem: cartItem[],
   setTotalPrice: any,
@@ -42,3 +43,45 @@ export const emptyCart = async (
 
   }
 }
+// 현재 유저 데이터 변경하기
+export const updateUserData = async (
+  setUser: any,
+  newUser: any
+  ) => {
+  await firebaseUpdateUser(newUser)
+    .then(() => {
+      setUser(newUser);
+    }).catch((error) => {
+      console.log(error);
+    })
+    .then(() => {
+      localStorage.setItem('user',JSON.stringify(newUser));
+      toast.success("데이터가 정상적으로 변경되었습니다.");
+    })
+}
+
+export const userCartItemsUpdate = (
+  uid: string,
+  items: cartItem[],
+  setItems: any
+) => {
+  const cartItems = items.filter((item: cartItem) => item.uid === uid);
+  setItems(cartItems);
+  return cartItems;
+}
+
+export const fetchUserCartData = async (user: any, setCartItems: any) => {
+  if (user) {
+    await firebaseFetchAllCartItems()
+      .then((data) => {
+        const userCart = userCartItemsUpdate(user.uid, data, setCartItems);
+        localStorage.setItem("cartItems", JSON.stringify(userCart));
+      })
+      .then(() => {})
+      .catch((e) => {
+        console.log(e);
+      });
+  } else {
+    localStorage.setItem("cartItems", "undefined");
+  }
+};
